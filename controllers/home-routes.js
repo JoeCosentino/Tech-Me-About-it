d7fb8fb73163b5df42d45d972756941ec3cd6086
Joey Cos <joeycosentino24@gmail.com>
2022-06-13 20:56:42 -0400

    users can now login and have a session stored in cookies, beginning logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: ['id', 'title', 'body', 'created_at'],
         include: [
@@ -32,6 +34,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
     res.render('login');
 });
 
