b05c68bc43863e2069679c0883b9a87bac0d3ffd
Joey Cos <joeycosentino24@gmail.com>
2022-06-14 21:34:31 -0400

    trying to create edit-post page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,7 +72,7 @@ router.post('/', withAuth, (req, res) => {
         });
 });
 
-router.post('/login', withAuth, (req, res) => {
+router.post('/login', (req, res) => {
     User.findOne({
         where: {
             email: req.body.email
